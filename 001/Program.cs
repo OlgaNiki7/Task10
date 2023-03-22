3db628c7ce9862ca6c52b6d2f6ee688504abe1b6
Olga Bazulina <nikitina.o.a@inbox.ru>
2023-03-22 23:02:46 +0300

    secondArray

---
+++ 001/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] array = new string[5] {"999", "99", "hello", "world", "cat"};
+string[] array1 = new string[array.Length];
+void SecondArray(string[] array, string[] array1)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        array1[count] = array[i];
+        count++;
+        }
+    }
+}
